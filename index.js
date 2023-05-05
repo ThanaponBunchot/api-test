4b80b28782924e3d6c3190d9b41c6c4ee8f40e37
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-05-05 22:15:46 +0700

    add filter

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ let { data, error } = await supabase
 app.post("/filterCategory",async(req,res)=>{
   let { data, error } = await supabase
 .from('news')
-.select('title')
+.select('title','content','imag_url','views')
 .textSearch('category','รถยนต์')
 return res.status(200).json({status:"success",count:data.length,result:data})
 
