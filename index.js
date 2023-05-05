261dfa45f72e9fd49fbebc2de91c714819cf1b61
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-05-05 22:51:36 +0700

    adjust file

--- index.js
+++ index.js
@@ -2,38 +2,15 @@ import supabase from "./config/supabase.js";
 import express from "express";
 import cors from "cors";
 
+import * as useNews from  "./src/controller/news.js"
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", async (req, res) => {
-  let { data, error } = await supabase.from("news").select("*");
-  return res
-    .status(200)
-    .json({ status: "success", count: data.length, result: data });
-});
-
-app.post("/filterCategory", async (req, res) => {
-  const category = req.body.category;
-  let { data, error } = await supabase
-    .from("news")
-    .select("title,content,imag_url,views")
-    .textSearch("category", category);
-  return res
-    .status(200)
-    .json({ status: "success", count: data.length, result: data });
-});
-
-app.post("/filterViews", async (req, res) => {
-  const view = req.body.greaterThan;
-  let { data, error } = await supabase
-    .from("news")
-    .select("title,content,imag_url,views")
-    .gt("views", view);
-  return res
-    .status(200)
-    .json({ status: "success", count: data.length, result: data });
-});
+app.get("/",useNews.fetchData);
+app.post("/filterCategory", useNews.filterCategory);
+app.post("/filterViews", useNews.filerViews);
 
 app.listen(5000, () => {
   console.log("Server start on port 5000");
