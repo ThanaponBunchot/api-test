9c4c6750395cddc380c7b852a393e950b92a446b
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-05-05 23:00:13 +0700

    adjust files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import supabase from "./config/supabase.js";
+import supabase from "./config/supabase.js";
 import express from "express";
 import cors from "cors";
 
@@ -8,7 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/",useNews.fetchData);
+app.get("/",async (req, res) => {
+  let { data, error } = await supabase.from("news").select("*");
+  return res
+    .status(200)
+    .json({ status: "success", count: data.length, result: data });
+});
 app.post("/filterCategory", useNews.filterCategory);
 app.post("/filterViews", useNews.filerViews);
 
