e047138fbb2313ef7cfb242b87cc4d9f7f07412a
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-05-05 23:03:59 +0700

    adjust files

--- index.js
+++ index.js
@@ -8,14 +8,36 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/",async (req, res) => {
+
+
+app.get("/", async (req, res) => {
   let { data, error } = await supabase.from("news").select("*");
   return res
     .status(200)
     .json({ status: "success", count: data.length, result: data });
 });
-app.post("/filterCategory", useNews.filterCategory);
-app.post("/filterViews", useNews.filerViews);
+
+app.post("/filterCategory", async (req, res) => {
+  const category = req.body.category;
+  let { data, error } = await supabase
+    .from("news")
+    .select("title,content,imag_url,views")
+    .textSearch("category", category);
+  return res
+    .status(200)
+    .json({ status: "success", count: data.length, result: data });
+});
+
+app.post("/filterViews", async (req, res) => {
+  const view = req.body.greaterThan;
+  let { data, error } = await supabase
+    .from("news")
+    .select("title,content,imag_url,views")
+    .gt("views", view);
+  return res
+    .status(200)
+    .json({ status: "success", count: data.length, result: data.sort((a,b)=>b.views - a.views) });
+});
 
 app.listen(5000, () => {
   console.log("Server start on port 5000");
