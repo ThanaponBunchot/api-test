07c1d2a5c5ee7a377196839db9feb5c0bc70c7ee
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-05-05 21:51:02 +0700

    add filter

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ let { data, error } = await supabase
 
 })
 
+
+app.post("/filterCategory",async(req,res)=>{
+  let { data, error } = await supabase
+.from('news')
+.select('*')
+.like('category', '%รถยนต์')
+return res.status(200).json({status:"success",count:data.length,result:data})
+
+})
+
+
 app.listen(5000, () => {
   console.log("Server start on port 5000");
 });
